f9340e769350ec4de26012e17fee670384917ec9
Lvzbel <rcoto@protonmail.com>
2020-05-10 01:07:38 -0700

    Feat: none responsive navbar complete

--- header.php
+++ header.php
@@ -9,3 +9,25 @@
 </head>
 
 <body <?php body_class();?>>
+
+  <header class="nav">
+    <div class="container nav__container">
+
+      <a href="#" class="nav__logo">
+        <span class="nav__brand">
+          Lucero
+        </span>Rent-A-Car
+      </a>
+
+      <!-- Display Main Navigation -->
+      <?php 
+          $args = array(
+            'theme_location' => 'main-menu',
+            'container' => 'nav',
+            'container_class' => 'nav__links'
+          );
+
+          wp_nav_menu( $args );
+        ?>
+    </div>
+  </header>
