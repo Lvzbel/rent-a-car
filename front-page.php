19d5a7d40f6deb9469ff6bb53878d2375540da20
Lvzbel <rcoto@protonmail.com>
2020-05-10 14:16:43 -0700

    Feat: mobile main cta completed

--- front-page.php
+++ front-page.php
@@ -4,10 +4,11 @@
 
 <section class="cta">
 
-  <h1 class="cta__header">Lucero Rent-A-Car
+  <h1 class="cta__header"><span class="cta__brandname">Lucero</span> Rent-A-Car
     <span class="cta__lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span>
   </h1>
-  <a class="btn btn-primary cta__btn" href="#">View our Inventory</a>
+  <a class="btn btn--primary cta__btn" href="#">View our Inventory</a>
+  <button class="btn btn--primary">Contact Us</button>
 
 </section>
 
