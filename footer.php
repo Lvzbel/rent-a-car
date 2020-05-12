2cbc532898a8b400d9ee1c85cdf833884457d524
Lvzbel <rcoto@protonmail.com>
2020-05-12 01:17:56 -0700

    Feat: footer completed

--- footer.php
+++ footer.php
@@ -1,4 +1,43 @@
 </main>
+
+<footer class="footer">
+  <div class="container">
+    <div class="footer__brand-box">
+      <a href="#" class="footer__logo">
+        <span class="footer__brand">
+          Lucero
+        </span>Rent-A-Car
+      </a>
+    </div>
+
+    <!-- Display Main Navigation -->
+    <?php 
+          $args = array(
+            'theme_location' => 'main-menu',
+            'container_class' => 'footer__links'
+          );
+
+          wp_nav_menu( $args );
+        ?>
+
+    <div class="footer__social">
+      <ul>
+        <li><a href="#"><i class="lab la-instagram"></i></a></li>
+        <li><a href="#"><i class="lab la-facebook-f"></i></a></li>
+        <li><a href="#"><i class="lab la-twitter"></i></a></li>
+      </ul>
+    </div>
+
+    <div class="footer__copyright">
+      <?php
+    function currentYear(){
+        return date('Y');
+    }
+    ?>
+      © <?php echo currentYear(); ?> All Rights Reserved.
+    </div>
+  </div>
+</footer>
 <?php wp_footer(); ?>
 
 </body>
