75d98dddd94327a24aeba94792282aabdca54ac7
Lvzbel <rcoto@protonmail.com>
2020-05-15 17:00:37 -0700

    Feat: blog preview component created

---
+++ components/blog-preview.php
@@ -0,0 +1,18 @@
+<div class="blog__item">
+  <div class="blog__img-box">
+    <a href="<?php the_permalink() ?>">
+      <?php the_post_thumbnail('medium_large') ?></a>
+  </div>
+
+  <div class="blog__content">
+    <h4><a class="blog__title" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+    <?php the_excerpt(); ?>
+  </div>
+  <div class="blog__footer">
+    <div class="blog__tags">
+      <?php the_tags(); ?>
+    </div>
+    <a href="<?php the_permalink() ?>" class="blog__link"><i class="las la-file-alt blog__link-icon"></i>Read
+      More</a>
+  </div>
+</div>
