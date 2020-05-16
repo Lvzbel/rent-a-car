8f288067c4c37b9963288c4dbad1bb73819182a3
Lvzbel <rcoto@protonmail.com>
2020-05-15 17:00:56 -0700

    Refact: implemented blog-preview component

--- front-page.php
+++ front-page.php
@@ -302,24 +302,7 @@
         $blog = new WP_Query($args);
         while($blog->have_posts()): $blog->the_post(); ?>
 
-      <div class="blog__item">
-        <div class="blog__img-box">
-          <a href="<?php the_permalink() ?>">
-            <?php the_post_thumbnail('medium_large') ?></a>
-        </div>
-
-        <div class="blog__content">
-          <h4><a class="blog__title" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
-          <?php the_excerpt(); ?>
-        </div>
-        <div class="blog__footer">
-          <div class="blog__tags">
-            <?php the_tags(); ?>
-          </div>
-          <a href="<?php the_permalink() ?>" class="blog__link"><i class="las la-file-alt blog__link-icon"></i>Read
-            More</a>
-        </div>
-      </div>
+      <?php get_template_part('components/blog-preview') ?>
 
       <?php endwhile; wp_reset_postdata();?>
     </div>
