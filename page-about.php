de59ada230630426fedecf533e1c18bcc315cae2
Lvzbel <rcoto@protonmail.com>
2020-05-20 20:50:58 -0700

    Feat: ACF has been integrated

--- page-about.php
+++ page-about.php
@@ -6,25 +6,35 @@
 
     <?php while(have_posts()): the_post(); ?>
 
-    <h1><?php the_title(); ?></h1>
+    <h1 class="header__secondary"><?php the_title(); ?></h1>
 
     <div class="about__content">
       <div class="about__section">
+        <h3 class="about__title"><?php the_field('section_1_title') ?></h3>
         <div class="about__img">
-          image goes here
+          <?php 
+          $image = get_field('section_1_image');
+          ?>
+          <img src="<?php echo $image['sizes']['large'] ?>" alt="">
         </div>
         <div class="about__text">
-          text goes here
+          <?php the_field('section_1_text') ?>
         </div>
       </div>
+      <?php if(get_field('section_2_title')): ?>
       <div class="about__section">
+        <h3 class="about__title"><?php the_field('section_2_title') ?></h3>
         <div class="about__text">
-          text goes here
+          <?php the_field('section_2_text') ?>
         </div>
         <div class="about__img">
-          image goes here
+          <?php 
+          $image = get_field('section_2_image');
+          ?>
+          <img src="<?php echo $image['sizes']['large'] ?>" alt="">
         </div>
       </div>
+      <?php endif; ?>
     </div>
 
 
